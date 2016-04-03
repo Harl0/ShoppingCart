2f549829136be7db65632e5d48e8d24d1db605a7
Jason Harris <jiharris90@gmail.com>
2016-04-03 13:39:43 +0100

    Added cost_of_items and basket_total functions.

--- bin/cart.sh
+++ bin/cart.sh
@@ -9,6 +9,7 @@
 #Initial Commit: 03/04/2016
 #Version: 	0.1 - Inital
 #			0.2 - Added common_procs and count_items function.
+#			0.3 - Added cost_of_items and basket_total functions.
 
 #set common values
 . /Users/Jason/Documents/practice/DDCT/bin/common_procs.sh
@@ -33,4 +34,28 @@ done < $items
 
 }
 
+function cost_of_items {
+
+	let "apple_cost_to_customer= $apples_count * $apple_value"
+	echo $total_apple_cost
+
+	let "orange_cost_to_customer= $orange_count * $orange_value"
+	echo $total_orange_cost
+
+}
+
+function basket_total {
+	printf "\n"
+	echo "##########"
+
+		let "basket_grand_total=$apple_cost_to_customer + $orange_cost_to_customer"
+
+	printf "Total: £%.2f" $(echo "scale=10;$basket_grand_total/100" | bc -l)
+	printf "\n"
+	echo "##########"
+
+}
+
 count_items
+cost_of_items
+basket_total
--- bin/common_procs.sh
+++ bin/common_procs.sh
@@ -1,1 +1,7 @@
 items="/Users/Jason/Documents/practice/DDCT/config/items.txt"
+
+apples_count=0
+oranges_count=0
+
+apple_value=60
+orange_value=25
