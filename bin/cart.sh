369b5c1790f46cd1b602b4c76e9f461393e5cb28
Jason Harris <jiharris90@gmail.com>
2016-04-03 13:36:29 +0100

    Added count_items function to cart.sh

--- bin/cart.sh
+++ bin/cart.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#set -x
 
 #Title: cart.sh
 #Developer: Jason Harris
@@ -7,3 +8,29 @@
 #Location: https://github.com/Harl0/ShoppingCart.git
 #Initial Commit: 03/04/2016
 #Version: 	0.1 - Inital
+#			0.2 - Added common_procs and count_items function.
+
+#set common values
+. /Users/Jason/Documents/practice/DDCT/bin/common_procs.sh
+
+function count_items {
+
+while read line
+do
+	if [[ $line == "apple" ]]
+	then
+	    echo "Found apple"
+	    let "apples_count=apples_count+1"
+	    echo "Found apple, count now:" $apples_count
+	else 
+		if [[ $line == "orange" ]]
+		then
+	    let "orange_count=orange_count+1"
+	    echo "Found orange, count now:" $orange_count
+	    fi       
+	fi
+done < $items
+
+}
+
+count_items
---
+++ bin/common_procs.sh
@@ -0,0 +1,1 @@
+items="/Users/Jason/Documents/practice/DDCT/config/items.txt"
