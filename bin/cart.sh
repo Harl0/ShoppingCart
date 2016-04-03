ba3bbf0cd2d358692df7a806a85a584f89e64f6e
Jason Harris <jiharris90@gmail.com>
2016-04-03 13:49:37 +0100

    Testing of cart.sh success.

--- bin/cart.sh
+++ bin/cart.sh
@@ -20,14 +20,11 @@ while read line
 do
 	if [[ $line == "apple" ]]
 	then
-	    echo "Found apple"
-	    let "apples_count=apples_count+1"
-	    echo "Found apple, count now:" $apples_count
+	   let "apples_count=apples_count+1"
 	else 
 		if [[ $line == "orange" ]]
 		then
-	    let "orange_count=orange_count+1"
-	    echo "Found orange, count now:" $orange_count
+	       let "orange_count=orange_count+1"
 	    fi       
 	fi
 done < $items
@@ -37,18 +34,14 @@ done < $items
 function cost_of_items {
 
 	let "apple_cost_to_customer= $apples_count * $apple_value"
-	echo $total_apple_cost
-
 	let "orange_cost_to_customer= $orange_count * $orange_value"
-	echo $total_orange_cost
 
 }
 
 function basket_total {
-	printf "\n"
 	echo "##########"
 
-		let "basket_grand_total=$apple_cost_to_customer + $orange_cost_to_customer"
+	   let "basket_grand_total=$apple_cost_to_customer + $orange_cost_to_customer"
 
 	printf "Total: £%.2f" $(echo "scale=10;$basket_grand_total/100" | bc -l)
 	printf "\n"
